ecad4204776aec8f0b5a52e2a24cfd522c794598
hyeonrok <wjsgusfhr@naver.com>
2024-03-06 10:13:05 +0900

    codegroup

--- test2/src/main/java/com/example/demo/infra/codegroup/CodeGroupService.java
+++ test2/src/main/java/com/example/demo/infra/codegroup/CodeGroupService.java
@@ -23,6 +23,27 @@ public class CodeGroupService {
 //		return list;
 //	}
 	
-	public List<CodeGroupDto> selectList(){ return dao.selectList();}
+	public List<CodeGroupDto> selectList(){
+		return dao.selectList();
+		}
+	
+	public CodeGroupDto selectOne(CodeGroupDto dto) {
+		return dao.selectOne(dto);
+	}
+	
+	public int insert(CodeGroupDto dto) {
+		return dao.insert(dto);
+	}
+	
+	public int update(CodeGroupDto dto) {
+		return dao.update(dto);
+	}
+	
+	public int uelete(CodeGroupDto dto) {
+		return dao.uelete(dto);
+	}
+	public int delete(CodeGroupDto dto) {
+		return dao.delete(dto);
+	}
 
 }
